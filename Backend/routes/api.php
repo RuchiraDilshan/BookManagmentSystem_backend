9ed08893053b89b50235145997d0d7e2a36650a3
Ruchira Dilshan Wijerathna <ruchiradilshan17@gmail.com>
2025-11-07 02:02:01 +0530

    create api routes

---
+++ Backend/routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\BorrowController;
+
+
+// Category Routes
+Route::get('/categories', [CategoryController::class, 'index']);
+
+// Book Routes
+Route::apiResource('books', BookController::class);
+
+// User Routes
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+
+
+// Borrow Routes
+
+Route::get('/borrow-records', [BorrowController::class, 'index']);
+Route::get('/borrow-records/current', [BorrowController::class, 'currentBorrows']);
+Route::get('/borrow-records/history', [BorrowController::class, 'borrowHistory']);
+Route::post('/borrow-records/borrow', [BorrowController::class, 'borrow']);
+Route::post('/borrow-records/return', [BorrowController::class, 'returnBook']);
+Route::post('/borrow-records/{id}/return', [BorrowController::class, 'returnBook']);

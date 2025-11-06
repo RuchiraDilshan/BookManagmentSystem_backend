922468b079e289b51815ab66676b412392ffb5e4
Ruchira Dilshan Wijerathna <ruchiradilshan17@gmail.com>
2025-11-06 23:21:01 +0530

    Create Store book validation request

---
+++ Backend/app/Http/Controllers/Requests/StoreBookRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'author' => 'required|string|max:255',
+            'price' => 'required|numeric|min:0',
+            'stock' => 'required|integer|min:0',
+            'book_category_id' => 'required|exists:book_categories,id',
+        ];
+    }
+}

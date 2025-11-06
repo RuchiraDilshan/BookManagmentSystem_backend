5413ca876a1c4321cf18646d2f0da0e0b47a5b91
Ruchira Dilshan Wijerathna <ruchiradilshan17@gmail.com>
2025-11-07 00:53:21 +0530

    Create user controller

---
+++ Backend/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+
+class UserController extends Controller
+{
+    //user list
+    public function index()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+
+    publicfunction store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json($user, 201);
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return response()->json($user);
+    }
+}

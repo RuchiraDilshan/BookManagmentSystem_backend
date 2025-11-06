16804d076d7811da9afdb2bfc41b2762c851b973
Ruchira Dilshan Wijerathna <ruchiradilshan17@gmail.com>
2025-11-06 23:29:12 +0530

    create category controller

---
+++ Backend/app/Http/Controllers/Api/categoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\BookCategory;;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    //category list
+    public function index()
+    {
+        $categories = BookCategory::all();
+        return response()->json($categories);
+    }
+
+}

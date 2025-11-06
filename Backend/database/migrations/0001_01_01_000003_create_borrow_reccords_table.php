d9be1b98738822e2e6401a118884a552e5dbb834
Ruchira Dilshan Wijerathna <ruchiradilshan17@gmail.com>
2025-11-06 13:15:49 +0530

    update migration

--- Backend/database/migrations/0001_01_01_000003_create_borrow_reccords_table.php
+++ Backend/database/migrations/0001_01_01_000003_create_borrow_reccords_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBorrowRecordsTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -23,4 +23,5 @@ class CreateBorrowRecordsTable extends Migration
     {
         Schema::dropIfExists('borrow_records');
     }
-}
+};
+
